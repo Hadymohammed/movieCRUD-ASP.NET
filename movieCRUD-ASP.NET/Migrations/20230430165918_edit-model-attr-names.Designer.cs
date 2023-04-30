e17fd6b66c301e7faff46e3d2dd9d1a1ec77d716
Hadymohammed <Hadymohamed2002@gmail.com>
2023-04-30 20:11:20 +0300

    edit tables attributes

--- movieCRUD-ASP.NET/Migrations/20230430165918_edit-model-attr-names.Designer.cs
+++ movieCRUD-ASP.NET/Migrations/20230430165918_edit-model-attr-names.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using movieCRUD_ASP.NET.Data;
 
@@ -11,9 +12,11 @@ using movieCRUD_ASP.NET.Data;
 namespace movieCRUD_ASP.NET.Migrations
 {
     [DbContext(typeof(AppDBcontext))]
-    partial class AppDBcontextModelSnapshot : ModelSnapshot
+    [Migration("20230430165918_edit-model-attr-names")]
+    partial class editmodelattrnames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ movieCRUD-ASP.NET/Migrations/20230430165918_edit-model-attr-names.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace movieCRUD_ASP.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class editmodelattrnames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Movies_MoiveId",
+                table: "Actors_Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "logoUrl",
+                table: "Cinemas",
+                newName: "Logo");
+
+            migrationBuilder.RenameColumn(
+                name: "MoiveId",
+                table: "Actors_Movies",
+                newName: "MovieId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Actors_Movies_MoiveId",
+                table: "Actors_Movies",
+                newName: "IX_Actors_Movies_MovieId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Producers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cinemas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Actors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Movies_MovieId",
+                table: "Actors_Movies",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Movies_MovieId",
+                table: "Actors_Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Logo",
+                table: "Cinemas",
+                newName: "logoUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieId",
+                table: "Actors_Movies",
+                newName: "MoiveId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Actors_Movies_MovieId",
+                table: "Actors_Movies",
+                newName: "IX_Actors_Movies_MoiveId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Producers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Cinemas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Actors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Movies_MoiveId",
+                table: "Actors_Movies",
+                column: "MoiveId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
